63a70534a2ce1d4067dd2962ab91a6f280cfa0f7
fatweedington <aron.binggeli@gmail.com>
2022-09-26 16:34:56 +0200

    City

--- src/main/kotlin/DoubleCase.kt
+++ src/main/kotlin/DoubleCase.kt
@@ -10,8 +10,8 @@ fun doubleCase(s:String):String{
 
 fun doubleCaseRec(s:String):String{
     val letters :String = s.filter {it.isLetter()}
-    if (letters.isNotEmpty()){
-    return letters.first().uppercase().plus(letters.first().lowercase()).plus(doubleCaseRec(letters.drop(1)))
+    return if (letters.isNotEmpty()){
+        letters.first().uppercase().plus(letters.first().lowercase()).plus(doubleCaseRec(letters.drop(1)))
     }
-    else return ""
+    else ""
 }
