c230ad258602e1009fc797b49911915f45a2b409
fatweedington <aron.binggeli@gmail.com>
2022-10-10 16:10:48 +0200

    ändu

--- src/main/kotlin/Pi.kt
+++
@@ -1,8 +0,0 @@
-class Pi {
-
-    fun computePi() :Double {
-        var base = 4.0
-
-        while ((3.0 - base))
-    }
-}
---
+++ src/main/kotlin/Ranges.kt
@@ -0,0 +1,22 @@
+import java.awt.font.NumericShaper
+import java.util.DoubleSummaryStatistics
+
+val intRange = 1..10
+
+    fun main(args: Array<String>) {
+        intRange.forEach { println(it) }
+        println(intRange.sum())
+        println(intRange.average())
+        println(getMedian(intRange))
+        println(getMedian((2..17) step(3)))
+    }
+
+    fun getMedian(range: IntProgression): Double {
+      var intarray = emptyArray<Int>()
+        range.forEach { intarray += it }
+        return if (intarray.size % 2 == 0) {
+            ((intarray[intarray.size / 2].toDouble() + intarray[intarray.size / 2 - 1].toDouble()) / 2)
+        } else {
+            (intarray[intarray.size / 2]).toDouble()
+        }
+    }
