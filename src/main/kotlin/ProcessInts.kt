d86f6f20a0a15c646e9657ba54b757008a5cb512
fatweedington <aron.binggeli@gmail.com>
2022-11-25 17:45:40 +0100

    ändu

--- src/main/kotlin/ProcessInts.kt
+++ src/main/kotlin/ProcessInts.kt
@@ -1,3 +1,11 @@
-fun process1Int(n: Int, action: (Int) -> Unit) = action(n)
+fun process1Int(n: Int, action: (Int) -> Int) =  action(n)
 
-fun process2Int(n1: Int,n2:  Int, action(n2): (Int) -> Unit) = n1.action(n2)
+fun process2Int(n1: Int, n2:  Int, action: (Int,Int) -> Int) = action(n1,n2)
+
+fun process3Int(n1: Int,n2:  Int, n3 : Int, action: (Int,Int) -> Int) =  action(n1,action(n2,n3))
+
+fun main() {
+    println(process1Int(5) { i -> i.inc() })
+    println(process2Int(3,5) { i, j -> i * j })
+    println(process3Int(3,5,6) { i,j -> i*j })
+}
